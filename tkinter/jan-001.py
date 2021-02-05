5626a0af256aad0390d26305bb68389772671f7d
solito83 <solito83@users.noreply.github.com>
2021-02-05 07:43:21 -0300

    j-002

--- tkinter/jan-001.py
+++ tkinter/jan-001.py
@@ -11,12 +11,16 @@ class application():
         window.mainloop()
     def tela(self):
         self.window.title('Ferramentas')
-        self.window.configure(background = 'red')
+        self.window.configure(background = '#b0e0e6')
         self.window.geometry('600x360')
         self.window.resizable(True, True)
         self.window.maxsize(width = 900, height = 600 )
         self.window.minsize(width = 400, height = 360)
     def frames_da_tela(self):
-        self.frame_1 = Frame(self.window)
+        self.frame_1 = Frame(self.window, bd = 4, bg = '#d3d3d3', highlightbackground = '#faf0e6', highlightthickness = 2)
+        self.frame_1.place(relx = 0.02, rely = 0.02, relwidth = 0.96, relheight = 0.47)
+        
+        self.frame_2 = Frame(self.window, bd = 4, bg = '#d3d3d3', highlightbackground = '#faf0e6', highlightthickness = 2)
+        self.frame_2.place(relx = 0.02, rely = 0.5, relwidth = 0.96, relheight = 0.47)
 
 application()
