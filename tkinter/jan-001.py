406ffe9bd7f5d7f7533e387629d398dbb4d8139c
solito83 <solito83@users.noreply.github.com>
2021-02-07 09:36:18 -0300

    j-005

--- tkinter/jan-001.py
+++ tkinter/jan-001.py
@@ -12,7 +12,7 @@ class application():
         window.mainloop()
     def tela(self):
         self.window.title('Ferramentas')
-        self.window.configure(background = '#13bd98')
+        self.window.configure(background = '#0099ff')
         self.window.geometry('700x500')
         self.window.resizable(True, True)
         self.window.maxsize(width = 1000, height = 600 )
@@ -20,10 +20,10 @@ class application():
     def frames_da_tela(self):
 # frame_1
         self.frame_1 = Frame(self.window, bd = 4, bg = '#d3d3d3', highlightbackground = '#faf0e6', highlightthickness = 2)
-        self.frame_1.place(relx = 0.02, rely = 0.02, relwidth = 0.96, relheight = 0.47)
+        self.frame_1.place(relx = 0.01, rely = 0.01, relwidth = 0.98, relheight = 0.43)
 # frame_2        
         self.frame_2 = Frame(self.window, bd = 4, bg = '#d3d3d3', highlightbackground = '#faf0e6', highlightthickness = 2)
-        self.frame_2.place(relx = 0.02, rely = 0.5, relwidth = 0.96, relheight = 0.47)
+        self.frame_2.place(relx = 0.01, rely = 0.45, relwidth = 0.98, relheight = 0.54)
     def botao_es(self):
 # botão limpar
         self.bt_limpar = Button(self.frame_1, text = 'Limpar', bd = 3, bg = '#d8a64b', fg = 'white', font = ('helvetica', 9, 'bold'))
@@ -45,54 +45,54 @@ class application():
         self.lb_codigo.place(relx = 0.1, rely = 0.15)
 
         self.codigo_entry = Entry(self.frame_1)
-        self.codigo_entry.place(relx = 0.2, rely = 0.15, relwidth = 0.2, relheight = 0.1)
+        self.codigo_entry.place(relx = 0.25, rely = 0.15, relwidth = 0.15, relheight = 0.1)
 # Label nome
         self.lb_nome = Label(self.frame_1, text = 'Nome', bg = '#d3d3d3', fg = '#0099ff')
         self.lb_nome.place(relx = 0.1, rely = 0.3)
 
         self.nome_entry = Entry(self.frame_1)
-        self.nome_entry.place(relx = 0.2, rely = 0.3, relwidth = 0.7, relheight = 0.1)
-# Label telefone
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.1, rely = 0.45)
+        self.nome_entry.place(relx = 0.25, rely = 0.3, relwidth = 0.65, relheight = 0.1)
+# Label marca
+        self.lb_marca = Label(self.frame_1, text = 'Marca', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_marca.place(relx = 0.1, rely = 0.45)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.2, rely = 0.45, relwidth = 0.2, relheight = 0.1)
-# Label cidade
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.45, rely = 0.45)
+        self.marca_entry = Entry(self.frame_1)
+        self.marca_entry.place(relx = 0.25, rely = 0.45, relwidth = 0.2, relheight = 0.1)
+# Label calibragem
+        self.lb_calibragem = Label(self.frame_1, text = 'Calibragem', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_calibragem.place(relx = 0.55, rely = 0.45)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.55, rely = 0.45, relwidth = 0.35, relheight = 0.1)
-# Label teleuuoo
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.1, rely = 0.6)
+        self.calibragem_entry = Entry(self.frame_1)
+        self.calibragem_entry.place(relx = 0.70, rely = 0.45, relwidth = 0.2, relheight = 0.1)
+# Label quantidade
+        self.lb_quantidade = Label(self.frame_1, text = 'Quantidade', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_quantidade.place(relx = 0.1, rely = 0.6)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.2, rely = 0.6, relwidth = 0.2, relheight = 0.1)
-# Label cidnnmm
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.45, rely = 0.6)
+        self.quantidade_entry = Entry(self.frame_1)
+        self.quantidade_entry.place(relx = 0.25, rely = 0.6, relwidth = 0.2, relheight = 0.1)
+# Label valor
+        self.lb_valor = Label(self.frame_1, text = 'Valor', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_valor.place(relx = 0.55, rely = 0.6)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.55, rely = 0.6, relwidth = 0.35, relheight = 0.1)
-# Label teleuuoo
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.1, rely = 0.75)
+        self.valor_entry = Entry(self.frame_1)
+        self.valor_entry.place(relx = 0.7, rely = 0.6, relwidth = 0.2, relheight = 0.1)
+# Label data de aquisição
+        self.lb_data_compra = Label(self.frame_1, text = 'Data-Entrada', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_data_compra.place(relx = 0.1, rely = 0.75)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.2, rely = 0.75, relwidth = 0.2, relheight = 0.1)
-# Label cidnnmm
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.45, rely = 0.75)
+        self.data_compra_entry = Entry(self.frame_1)
+        self.data_compra_entry.place(relx = 0.25, rely = 0.75, relwidth = 0.2, relheight = 0.1)
+# Label data de baixa
+        self.lb_baixa = Label(self.frame_1, text = 'Baixa', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_baixa.place(relx = 0.55, rely = 0.75)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.55, rely = 0.75, relwidth = 0.35, relheight = 0.1)
-# Label teleuuoo
-        self.lb_telefone = Label(self.frame_1, text = 'Telefone', bg = '#d3d3d3', fg = '#0099ff')
-        self.lb_telefone.place(relx = 0.1, rely = 0.90)
+        self.data_baixa_entry = Entry(self.frame_1)
+        self.data_baixa_entry.place(relx = 0.70, rely = 0.75, relwidth = 0.2, relheight = 0.1)
+# Comentários
+        self.lb_comentario = Label(self.frame_1, text = 'Comentarios', bg = '#d3d3d3', fg = '#0099ff')
+        self.lb_comentario.place(relx = 0.1, rely = 0.90)
 
-        self.telefone_entry = Entry(self.frame_1)
-        self.telefone_entry.place(relx = 0.2, rely = 0.90, relwidth = 0.7, relheight = 0.1)
+        self.comentario_entry = Entry(self.frame_1)
+        self.comentario_entry.place(relx = 0.25, rely = 0.90, relwidth = 0.65, relheight = 0.1)
 
 application()
--- tkinter/setup.py
+++ tkinter/setup.py
@@ -1,14 +1,13 @@
-# A simple setup script to create an executable using Tkinter. This also
-# demonstrates the method for creating a Windows executable that does not have
-# an associated console.
-#
-# SimpleTkApp.py is a very simple type of Tkinter application
-#
-# Run the build process by running the command 'python setup.py build'
-#
-# If everything works well you should find a subdirectory in the build
-# subdirectory that contains the files needed to run the application
-# python setup.py build
+# Comentarios
+'''
+    # A simple setup script to create an executable using Tkinter. This also
+    # demonstrates the method for creating a Windows executable that does not have
+    # an associated console.
+    # SimpleTkApp.py is a very simple type of Tkinter application
+    # Run the build process by running the command 'python setup.py build'
+    # If everything works well you should find a subdirectory in the build
+    # subdirectory that contains the files needed to run the application
+'''
 
 import sys
 from cx_Freeze import setup, Executable
@@ -24,4 +23,4 @@ setup(
     version="0.1",
     description="Controle de ferramentas",
     executables=executables,
-)
+    )
